46d35ffa8b704c7e57f3a235d298a19abe18afe6
HectorBL04 <hmanuelbloopez@gmail.com>
2018-03-11 23:10:34 -0600

    Version 0.005

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
 	<title>PAGINA PRINCIPAL</title>
 </head>
 <body>
+	<h1> PRIMER MVC </h1>
 <?
 echo "Hola Mundo";
 
 
-echo "nueva version 0.004"
+echo "  nueva version 0.004  "
 
 
 
 ?>
+<footer>
+	
+	<span>   github/HectorBL04 </span>
+
+</footer>
 </body>
 </html>
 
